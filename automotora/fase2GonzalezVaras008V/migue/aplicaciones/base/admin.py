d72a834214a75c6263bd5ca4faa0c85eb2d453f8
unknown <walterespinosaponce@gmail.com>
2020-11-08 00:57:36 -0300

    Creando sitio version 2

---
+++ automotora/fase2GonzalezVaras008V/migue/aplicaciones/base/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Categoria)
+admin.site.register(Cliente)
+admin.site.register(Post)
+admin.site.register(Web)
+admin.site.register(RedesSociales)
+admin.site.register(Contacto)
